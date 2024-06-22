e5616c34978b7289a4cf1029818b6b4d1cb43419
Kai Lin <klin@sdsc.edu>
2024-06-21 21:54:49 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+
+from fastapi import FastAPI
+from fastapi_utils.timing import add_timing_middleware
+from starlette.middleware.cors import CORSMiddleware
+
+from routes import ontomap
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title=f"Ontology Matching API",
+    description="Ontology Matching API.",
+    version="0.1",
+    docs_url="/docs",
+    openapi_url=f"/v1/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+add_timing_middleware(app, record=logger.info, prefix="app", exclude="untimed")
+
+app.include_router(ontomap.router)
+
